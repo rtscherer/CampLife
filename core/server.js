f3d05cae7e01f329b4484499474869859ec8a1e0
Ryan Scherer <rtscherer@gmail.com>
2017-02-12 01:10:36 -0500

    Updating server.js file for heroku

--- core/server.js
+++ core/server.js
@@ -5,12 +5,9 @@ var config = require('../config');
 // start the server
 module.exports = {
     start: function() {
-        //app.listen(config.port);
-        //app.set('port', (process.env.PORT || 5000));
-        var server = app.listen(process.env.PORT || 8080, function () {
+        app.listen(process.env.PORT || 8080, function () {
           var port = server.address().port;
-          console.log("App now running on port", port);
+          console.log("RUNNING: App now running on port", port);
         });
-        console.log('RUNNING: api running on port: ' + config.port);
     }
 };
