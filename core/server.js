7fedc44c1316a1f629396534b4d3b000958cbb1c
Ryan Scherer <rtscherer@gmail.com>
2017-02-12 00:59:39 -0500

    Updating port set for heroku

--- core/server.js
+++ core/server.js
@@ -5,7 +5,8 @@ var config = require('../config');
 // start the server
 module.exports = {
     start: function() {
-        app.listen(config.port);
+        //app.listen(config.port);
+        app.set('port', (process.env.PORT || 5000));
         console.log('RUNNING: api running on port: ' + config.port);
     }
 };
