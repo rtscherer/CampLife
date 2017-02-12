51f5d7dd40f6f2ca7c8d9caad7fd77cfba056fe9
Ryan Scherer <rtscherer@gmail.com>
2017-02-12 01:07:22 -0500

    Updating server.js to obtain port correctly for heroku

--- core/server.js
+++ core/server.js
@@ -6,7 +6,11 @@ var config = require('../config');
 module.exports = {
     start: function() {
         //app.listen(config.port);
-        app.set('port', (process.env.PORT || 5000));
+        //app.set('port', (process.env.PORT || 5000));
+        var server = app.listen(process.env.PORT || 8080, function () {
+          var port = server.address().port;
+          console.log("App now running on port", port);
+        });
         console.log('RUNNING: api running on port: ' + config.port);
     }
 };
